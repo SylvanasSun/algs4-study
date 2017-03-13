480a75237665464356d625ce291769eef8c2178a
SylvanasSun <sylvanassun_xtz@163.com>
2017-03-13 08:57:01 +0800

    add chapter2-2.1 ElementarySorts Shell Sort

---
+++ src/main/java/chapter2_sorting/C2_1_ElementarySorts/Shell.java
@@ -0,0 +1,123 @@
+package chapter2_sorting.C2_1_ElementarySorts;
+
+/******************************************************************************
+ *  Compilation:  javac Shell.java
+ *  Execution:    java Shell < input.txt
+ *  Dependencies: StdOut.java StdIn.java
+ *  Data files:   http://algs4.cs.princeton.edu/21sort/tiny.txt
+ *                http://algs4.cs.princeton.edu/21sort/words3.txt
+ *
+ *  Sorts a sequence of strings from standard input using shellsort.
+ *
+ *  Uses increment sequence proposed by Sedgewick and Incerpi.
+ *  The nth element of the sequence is the smallest integer >= 2.5^n
+ *  that is relatively prime to all previous terms in the sequence.
+ *  For example, incs[4] is 41 because 2.5^4 = 39.0625 and 41 is
+ *  the next integer that is relatively prime to 3, 7, and 16.
+ *
+ *  % more tiny.txt
+ *  S O R T E X A M P L E
+ *
+ *  % java Shell < tiny.txt
+ *  A E E L M O P R S T X                 [ one string per line ]
+ *
+ *  % more words3.txt
+ *  bed bug dad yes zoo ... all bad yet
+ *
+ *  % java Shell < words3.txt
+ *  all bad bed bug dad ... yes yet zoo    [ one string per line ]
+ *
+ *
+ ******************************************************************************/
+
+
+import java.util.Scanner;
+
+/**
+ * 希尔排序的思想是使数组中任意间隔为h的元素都是有序的.
+ * 它交换不相邻的元素以对数组的局部进行排序,并最终用插入排序将局部有序的数组排序.
+ * <p>
+ * Created by SylvanasSun on 2017/3/13.
+ */
+public class Shell {
+
+    private Shell() {
+    }
+
+    /**
+     * 按升序进行重排序,使用自然顺序(需要目标数组实现Comparable接口)
+     *
+     * @param a 要排序的数组
+     */
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h < N / 3)
+            h = 3 * h + 1; //1,4,13,40,121,364,1093,...
+        while (h >= 1) {
+            //将数组变为h有序
+            for (int i = h; i < N; i++) {
+                //将a[i]插入到a[i-h],a[i-2*h],a[i-3*h]....之中
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    exch(a, j, j - h);
+                }
+            }
+            assert isHsorted(a, h);
+            h = h / 3;
+        }
+        assert isSorted(a);
+    }
+
+
+    /***************************************************************************
+     *  Helper sorting functions.
+     ***************************************************************************/
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+    /***************************************************************************
+     *  Check if array is sorted - useful for debugging.
+     ***************************************************************************/
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i - 1])) return false;
+        return true;
+    }
+
+    // is the array h-sorted?
+    private static boolean isHsorted(Comparable[] a, int h) {
+        for (int i = h; i < a.length; i++)
+            if (less(a[i], a[i - h])) return false;
+        return true;
+    }
+
+    // print array to standard output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    /**
+     * Reads in a sequence of strings from standard input; Shellsorts them;
+     * and prints them to standard output in ascending order.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        String[] a = new Scanner(System.in).nextLine().split("\\s+");
+        Shell.sort(a);
+        show(a);
+    }
+}
