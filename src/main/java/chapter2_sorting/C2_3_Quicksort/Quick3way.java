6f60f55e5a2d71093132268023cde9adba41fdde
SylvanasSun <sylvanassun_xtz@163.com>
2017-03-14 17:06:26 +0800

    add chapter2-2.3 Quicksort Quick3way Sort

---
+++ src/main/java/chapter2_sorting/C2_3_Quicksort/Quick3way.java
@@ -0,0 +1,122 @@
+package chapter2_sorting.C2_3_Quicksort;
+
+/******************************************************************************
+ *  Compilation:  javac Quick3way.java
+ *  Execution:    java Quick3way < input.txt
+ *  Dependencies: StdOut.java StdIn.java
+ *  Data files:   http://algs4.cs.princeton.edu/23quicksort/tiny.txt
+ *                http://algs4.cs.princeton.edu/23quicksort/words3.txt
+ *
+ *  Sorts a sequence of strings from standard input using 3-way quicksort.
+ *
+ *  % more tiny.txt
+ *  S O R T E X A M P L E
+ *
+ *  % java Quick3way < tiny.txt
+ *  A E E L M O P R S T X                 [ one string per line ]
+ *
+ *  % more words3.txt
+ *  bed bug dad yes zoo ... all bad yet
+ *
+ *  % java Quick3way < words3.txt
+ *  all bad bed bug dad ... yes yet zoo    [ one string per line ]
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * 三路快排(使用了三向切分)
+ * <p>
+ * Created by SylvanasSun on 2017/3/14.
+ */
+public class Quick3way {
+
+    private Quick3way() {
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+        assert isSorted(a);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        //维护指针lt使得a[lo..lt-1]中的元素都小于v
+        //维护指针gt使得a[gt+1..hi]中的元素都大于v
+        //维护指针i使得a[lt..i-1]中的元素都等于v,a[i..gt]中的元素都还未确定
+        int lt = lo, i = lo + 1, gt = hi;
+        Comparable v = a[lo];
+        //a[i]小于v,将a[lt]和a[i]交换,将lt和i + 1
+        //a[i]大于v,将a[gt]和a[i]交换,将gt - 1
+        //a[i]等于v,将i + 1
+        //为了使 a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]成立
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) {
+                exch(a, lt++, i++);
+            } else if (cmp > 0) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
+        }
+        sort(a, lo, lt - 1);
+        sort(a, gt + 1, hi);
+        assert isSorted(a, lo, hi);
+    }
+
+
+    /***************************************************************************
+     *  Helper sorting functions.
+     ***************************************************************************/
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+    /***************************************************************************
+     *  Check if array is sorted - useful for debugging.
+     ***************************************************************************/
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i - 1])) return false;
+        return true;
+    }
+
+
+    // print array to standard output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+    /**
+     * Reads in a sequence of strings from standard input; 3-way
+     * quicksorts them; and prints them to standard output in ascending order.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        Quick3way.sort(a);
+        show(a);
+    }
+}
