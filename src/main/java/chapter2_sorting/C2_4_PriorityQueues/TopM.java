cef99e9b07b7ef16d683b3e2f3ae7ee17bbdf130
SylvanasSun <sylvanassun_xtz@163.com>
2017-03-15 11:16:40 +0800

    add chapter2-2.4 PriorityQueues max heap priority queue and min heap priority queue

---
+++ src/main/java/chapter2_sorting/C2_4_PriorityQueues/TopM.java
@@ -0,0 +1,75 @@
+package chapter2_sorting.C2_4_PriorityQueues;
+
+/******************************************************************************
+ *  Compilation:  javac TopM.java
+ *  Execution:    java TopM m < input.txt
+ *  Dependencies: MinPQ.java Transaction.java StdIn.java StdOut.java
+ *  Data files:   http://algs4.cs.princeton.edu/24pq/tinyBatch.txt
+ *
+ *  Given an integer m from the command line and an input stream where
+ *  each line contains a String and a long value, this MinPQ client
+ *  prints the m lines whose numbers are the highest.
+ *
+ *  % java TopM 5 < tinyBatch.txt
+ *  Thompson    2/27/2000  4747.08
+ *  vonNeumann  2/12/1994  4732.35
+ *  vonNeumann  1/11/1999  4409.74
+ *  Hoare       8/18/1992  4381.21
+ *  vonNeumann  3/26/2002  4121.85
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Transaction;
+
+/**
+ * The {@code TopM} class provides a client that reads a sequence of
+ * transactions from standard input and prints the <em>m</em> largest ones
+ * to standard output. This implementation uses a {@link MinPQ} of size
+ * at most <em>m</em> + 1 to identify the <em>M</em> largest transactions
+ * and a {@link Stack} to output them in the proper order.
+ * <p>
+ * For additional documentation, see <a href="http://algs4.cs.princeton.edu/24pq">Section 2.4</a>
+ * of <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
+public class TopM {
+
+    // This class should not be instantiated.
+    private TopM() {
+    }
+
+    /**
+     * Reads a sequence of transactions from standard input; takes a
+     * command-line integer m; prints to standard output the m largest
+     * transactions in descending order.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        MinPQ<Transaction> pq = new MinPQ<Transaction>(m + 1);
+
+        while (StdIn.hasNextLine()) {
+            // Create an entry from the next line and put on the PQ.
+            String line = StdIn.readLine();
+            Transaction transaction = new Transaction(line);
+            pq.insert(transaction);
+
+            // remove minimum if m+1 entries on the PQ
+            if (pq.size() > m)
+                pq.delMin();
+        }   // top m entries are on the PQ
+
+        // print entries on PQ in reverse order
+        Stack<Transaction> stack = new Stack<Transaction>();
+        for (Transaction transaction : pq)
+            stack.push(transaction);
+        for (Transaction transaction : stack)
+            StdOut.println(transaction);
+    }
+}
