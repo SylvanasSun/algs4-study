c5b0cca251b905039717038eb2aa1e66076fab5c
SylvanasSun <sylvanassun_xtz@163.com>
2017-07-27 15:21:11 +0800

    Add chapter4-4.4 ShortestPaths: AcyclicLP.java

---
+++ src/main/java/chapter4_graphs/C4_4_ShortestPaths/AcyclicLP.java
@@ -0,0 +1,170 @@
+package chapter4_graphs.C4_4_ShortestPaths;
+
+/******************************************************************************
+ *  Compilation:  javac AcyclicLP.java
+ *  Execution:    java AcyclicP V E
+ *  Dependencies: EdgeWeightedDigraph.java DirectedEdge.java Topological.java
+ *  Data files:   http://algs4.cs.princeton.edu/44sp/tinyEWDAG.txt
+ *
+ *  Computes longeset paths in an edge-weighted acyclic digraph.
+ *
+ *  Remark: should probably check that graph is a DAG before running
+ *
+ *  % java AcyclicLP tinyEWDAG.txt 5
+ *  5 to 0 (2.44)  5->1  0.32   1->3  0.29   3->6  0.52   6->4  0.93   4->0  0.38
+ *  5 to 1 (0.32)  5->1  0.32
+ *  5 to 2 (2.77)  5->1  0.32   1->3  0.29   3->6  0.52   6->4  0.93   4->7  0.37   7->2  0.34
+ *  5 to 3 (0.61)  5->1  0.32   1->3  0.29
+ *  5 to 4 (2.06)  5->1  0.32   1->3  0.29   3->6  0.52   6->4  0.93
+ *  5 to 5 (0.00)
+ *  5 to 6 (1.13)  5->1  0.32   1->3  0.29   3->6  0.52
+ *  5 to 7 (2.43)  5->1  0.32   1->3  0.29   3->6  0.52   6->4  0.93   4->7  0.37
+ *
+ ******************************************************************************/
+
+import chapter4_graphs.C4_2_DirectedGraphs.Topological;
+import edu.princeton.cs.algs4.Stack;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * The {@code AcyclicLP} class represents a data type for solving the
+ * single-source longest paths problem in edge-weighted directed
+ * acyclic graphs (DAGs). The edge weights can be positive, negative, or zero.
+ * <p>
+ * This implementation uses a topological-sort based algorithm.
+ * The constructor takes time proportional to <em>V</em> + <em>E</em>,
+ * where <em>V</em> is the number of vertices and <em>E</em> is the number of edges.
+ * Afterwards, the {@code distTo()} and {@code hasPathTo()} methods take
+ * constant time and the {@code pathTo()} method takes time proportional to the
+ * number of edges in the longest path returned.
+ * <p>
+ * For additional documentation,
+ * see <a href="http://algs4.cs.princeton.edu/44sp">Section 4.4</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
+public class AcyclicLP {
+    private double[] distTo;          // distTo[v] = distance  of longest s->v path
+    private DirectedEdge[] edgeTo;    // edgeTo[v] = last edge on longest s->v path
+
+    /**
+     * Computes a longest paths tree from {@code s} to every other vertex in
+     * the directed acyclic graph {@code G}.
+     *
+     * @param G the acyclic digraph
+     * @param s the source vertex
+     * @throws IllegalArgumentException if the digraph is not acyclic
+     * @throws IllegalArgumentException unless {@code 0 <= s < V}
+     */
+    public AcyclicLP(EdgeWeightedDigraph G, int s) {
+        distTo = new double[G.vertex()];
+        edgeTo = new DirectedEdge[G.vertex()];
+
+        validateVertex(s);
+
+        for (int v = 0; v < G.vertex(); v++)
+            distTo[v] = Double.NEGATIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        // relax vertices in toplogical order
+        Topological topological = new Topological(G);
+        if (!topological.hasOrder())
+            throw new IllegalArgumentException("Digraph is not acyclic.");
+        for (int v : topological.order()) {
+            for (DirectedEdge e : G.adj(v))
+                relax(e);
+        }
+    }
+
+    // relax edge e, but update if you find a *longer* path
+    private void relax(DirectedEdge e) {
+        int v = e.from(), w = e.to();
+        if (distTo[w] < distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+        }
+    }
+
+    /**
+     * Returns the length of a longest path from the source vertex {@code s} to vertex {@code v}.
+     *
+     * @param v the destination vertex
+     * @return the length of a longest path from the source vertex {@code s} to vertex {@code v};
+     * {@code Double.NEGATIVE_INFINITY} if no such path
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public double distTo(int v) {
+        validateVertex(v);
+        return distTo[v];
+    }
+
+    /**
+     * Is there a path from the source vertex {@code s} to vertex {@code v}?
+     *
+     * @param v the destination vertex
+     * @return {@code true} if there is a path from the source vertex
+     * {@code s} to vertex {@code v}, and {@code false} otherwise
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public boolean hasPathTo(int v) {
+        validateVertex(v);
+        return distTo[v] > Double.NEGATIVE_INFINITY;
+    }
+
+    /**
+     * Returns a longest path from the source vertex {@code s} to vertex {@code v}.
+     *
+     * @param v the destination vertex
+     * @return a longest path from the source vertex {@code s} to vertex {@code v}
+     * as an iterable of edges, and {@code null} if no such path
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<DirectedEdge> pathTo(int v) {
+        validateVertex(v);
+        if (!hasPathTo(v)) return null;
+        Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.push(e);
+        }
+        return path;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = distTo.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+    }
+
+    /**
+     * Unit tests the {@code AcyclicLP} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        InputStream inputStream
+                = AcyclicLP.class.getResourceAsStream("/graph_file/C4_4_ShortestPaths/" + args[0]);
+        Scanner scanner = new Scanner(inputStream);
+        EdgeWeightedDigraph digraph = new EdgeWeightedDigraph(scanner);
+        Integer s = Integer.valueOf(args[1]);
+        AcyclicLP lp = new AcyclicLP(digraph, s);
+
+
+        for (int v = 0; v < digraph.vertex(); v++) {
+            if (lp.hasPathTo(v)) {
+                System.out.printf("%d to %d (%.2f)  ", s, v, lp.distTo(v));
+                for (DirectedEdge e : lp.pathTo(v)) {
+                    System.out.print(e + "   ");
+                }
+                System.out.println();
+            } else {
+                System.out.printf("%d to %d         no path\n", s, v);
+            }
+        }
+    }
+}
+
