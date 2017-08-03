b206fbb0f3b5fd8acc30aff0c4af9c1d2e494f0f
SylvanasSun <sylvanassun_xtz@163.com>
2017-08-03 12:01:11 +0800

    Add chapter5-5.1 StringSorts: TrieST.java

---
+++ src/main/java/chapter5_strings/C5_2_Tries/TrieST.java
@@ -0,0 +1,162 @@
+package chapter5_strings.C5_2_Tries;
+
+/******************************************************************************
+ *  Compilation:  javac TrieST.java
+ *  Execution:    java TrieST < words.txt
+ *  Dependencies: StdIn.java
+ *  Data files:   http://algs4.cs.princeton.edu/52trie/shellsST.txt
+ *
+ *  A string symbol table for extended ASCII strings, implemented
+ *  using a 256-way trie.
+ *
+ *  % java TrieST < shellsST.txt
+ *  by 4
+ *  sea 6
+ *  sells 1
+ *  she 0
+ *  shells 3
+ *  shore 7
+ *  the 5
+ *
+ ******************************************************************************/
+
+/**
+ * The {@code TrieST} class represents an symbol table of key-value
+ * pairs, with string keys and generic values.
+ * It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ * <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
+ * It also provides character-based methods for finding the string
+ * in the symbol table that is the <em>longest prefix</em> of a given prefix,
+ * finding all strings in the symbol table that <em>start with</em> a given prefix,
+ * and finding all strings in the symbol table that <em>match</em> a given pattern.
+ * A symbol table implements the <em>associative array</em> abstraction:
+ * when associating a value with a key that is already in the symbol table,
+ * the convention is to replace the old value with the new value.
+ * Unlike {@link java.util.Map}, this class uses the convention that
+ * values cannot be {@code null}—setting the
+ * value associated with a key to {@code null} is equivalent to deleting the key
+ * from the symbol table.
+ * <p>
+ * This implementation uses a 256-way trie.
+ * The <em>put</em>, <em>contains</em>, <em>delete</em>, and
+ * <em>longest prefix</em> operations take time proportional to the length
+ * of the key (in the worst case). Construction takes constant time.
+ * The <em>size</em>, and <em>is-empty</em> operations take constant time.
+ * Construction takes constant time.
+ * <p>
+ * For additional documentation, see <a href="http://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ */
+public class TrieST<Value> {
+
+    // extended ASCII
+    private static final int R = 256;
+
+    // root of trie
+    private Node root;
+
+    // number of keys in trie
+    private int N;
+
+    // Trie Node
+    private static class Node {
+        private Object value;
+        private Node[] next = new Node[R];
+    }
+
+    /**
+     * Initializes an empty string symbol table.
+     */
+    public TrieST() {
+    }
+
+    /**
+     * Returns the value associated with the given key.
+     *
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     * and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(String key) {
+        validateKey(key, "argument to get() is null.");
+
+        Node x = get(root, key, 0);
+        if (x == null) return null;
+        return (Value) x.value;
+    }
+
+    private Node get(Node x, String key, int d) {
+        if (x == null) return null;
+        if (d == key.length()) return x;
+        char c = key.charAt(d);
+        return get(x.next[c], key, d + 1);
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     *
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     * {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(String key) {
+        validateKey(key, "argument to contains() is null.");
+        return get(key) != null;
+    }
+
+    /**
+     * Removes the key from the set if the key is present.
+     *
+     * @param key the key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void delete(String key) {
+        validateKey(key, "argument to delete() is null.");
+        root = delete(root, key, 0);
+    }
+
+    private Node delete(Node x, String key, int d) {
+        if (x == null) return null;
+        if (d == key.length()) {
+            if (x.value != null) N--;
+            x.value = null;
+        } else {
+            char c = key.charAt(d);
+            x.next[c] = delete(x.next[c], key, d + 1);
+        }
+
+        // remove subtrie rooted at x if it is completely empty
+        if (x.value != null) return x;
+        for (int c = 0; c < R; c++) {
+            if (x.next[c] != null)
+                return x;
+        }
+        return null;
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return N;
+    }
+
+    /**
+     * Is this symbol table empty?
+     *
+     * @return {@code true} if this symbol table is empty and {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    private void validateKey(String key, String message) {
+        if (key == null)
+            throw new IllegalArgumentException(message);
+    }
+
+}
